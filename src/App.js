bf4355925f121878bb9a4714ec89e170f52b9c77
Constantina Scoullis <scoullisconstantina@gmail.com>
2022-01-19 21:39:06 -0800

    Added bonus objective, some styling changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import jiujitsuPositions from './default-starting-positons.json'
 import Button from './Button.js';
+import { bonusObjectives } from './constants'
 
 class App extends React.Component {
 
@@ -12,7 +13,9 @@ class App extends React.Component {
       description: {
         offense: "",
         defense: ""
-      }
+      },
+      offenseBonus: "",
+      defenseBonus: ""
     };
     
     this.generateStartingPosition = this.generateStartingPosition.bind(this)
@@ -26,6 +29,16 @@ class App extends React.Component {
       }
     }
     this.setState(newPosition);
+    this.generateBonusObjective();
+  }
+
+  generateBonusObjective(){
+    const offenseBonus = bonusObjectives[Math.floor(Math.random()*bonusObjectives.length)];
+    const defenseBonus = bonusObjectives[Math.floor(Math.random()*bonusObjectives.length)];
+    this.setState({
+      offenseBonus,
+      defenseBonus
+    });
   }
 
   render() {
@@ -34,15 +47,22 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <h1>The "Wheel" of Positional Sparring</h1>
+          <section id="instructions">
+            <ol>
+              <li>Click the button to spin the wheel. </li>
+              <li>You will then get a random sparring position with specific objectives for each partner.</li>
+              <li> If at any point you want a new position, click the button again.</li>
+            </ol>
+          </section>
         </header>
-  
         <div className="the-wheel">
           <h2>{this.state.positionName}</h2>
           <p className="offenseInstructions"> {this.state.description.offense !== "" && <strong>Offense:</strong>} {this.state.description.offense}</p>
+          <p className="bonusOffense"> {this.state.offenseBonus !== "" && <em>Offense Bonus Objective: attempt submission via {this.state.offenseBonus}</em>}</p>
           <p className="defenseInstructions"> {this.state.description.defense !== "" && <strong>Defense:</strong>} {this.state.description.defense}</p>
+          <p className="bonusDefense"> {this.state.defenseBonus !== "" && <em>Defense Bonus Objective: attempt submission via {this.state.defenseBonus}</em>}</p>
+          <Button onClick={this.generateStartingPosition} textContent={'Spin the Wheel'}></Button>
         </div>
-
-        <Button onClick={this.generateStartingPosition} textContent={'Spin the Wheel'}></Button>
       </div>
     );
   }
